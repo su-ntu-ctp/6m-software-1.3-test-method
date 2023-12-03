e0e9bd98da94fb189f337dcd43a77bbb67f44ba0
nhkhai@gmail.com <nhkhai@gmail.com>
2023-12-03 16:04:58 +0800

    Updated with my answers.

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -6,10 +6,27 @@
     - If `env` is "PROD", modify `databaseCredential` to "produser:password"
     
 */
-
-const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PROD"
+const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PROD".
 let databaseCredential = "devuser:password";
 
-// Task: Add code here
+// Implement a switch statement to check against `env` environmental variable.
+switch (env) {
+  // If `env` is "STAGE", modify `databaseCredential` to "stageuser:password".
+  case "STAGE":
+    databaseCredential = "stageuser:password";
+    break;
 
-console.log(`Database credential for environment ${env} is ${databaseCredential}`);
+  // If `env` is "PROD", modify `databaseCredential` to "produser:password"
+  case "PROD":
+    databaseCredential = "produser:password";
+    break;
+
+  // If `env` is "DEV", no change to `databaseCredential`.
+  case "DEV":
+  default:
+    break;
+}
+
+console.log(
+  `Database credential for environment ${env} is ${databaseCredential}`
+);
