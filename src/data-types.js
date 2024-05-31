c4b94d5e30f6d09577a5c818f464cac2d5db548d
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-05-31 15:20:32 +0800

    Update data-types.js

--- src/data-types.js
+++ src/data-types.js
@@ -1,3 +1,4 @@
+//data types
 const num = 1;
 console.log("num",typeof num);
 
@@ -18,3 +19,24 @@ console.log("obj",typeof obj);
 
 const arr = [];
 console.log("arr",typeof arr);
+
+//control structures:
+let number = 10;
+if (number % 2 ==0) {
+  console.log(number + " is even");
+} else {
+  console.log(number + " is odd");
+}
+
+let testnum1 = 0;
+while (testnum1 < 5){
+  console.log(testnum1);
+  testnum1++;
+}
+
+let testnum2 = 5; 
+do {
+  console.log(testnum2)
+  testnum2--;
+} while (testnum2 >= 0)
+  
