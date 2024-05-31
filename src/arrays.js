57d108c355dea7312a29ec400c97338771ef39db
terencegaffudSU <99375248+terencegaffudSU@users.noreply.github.com>
2024-05-31 14:53:40 +0800

    Update arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -12,3 +12,26 @@ console.log("first element value is", defaults[0]);
 for(let i=0 ;i<defaults.length; i++){
     console.log("value of i is", i, "and the value is", defaults[i]);
 }
+
+/*Class discussion
+Array methods:
+Break out into groups and research how each of the array methods can be used and give an example.
+1. push
+2. pop
+3. shift
+4. unshift
+5. splice
+6. slice
+*/
+
+//Exercise:
+/*
+Question 1:
+1. Create an array called grades with 5 numeric values
+2. Use a for loop to loop through the array to get the average of the values.
+
+Question 2:
+1. Use array methods to combine the two arrays and remove all even-indexed values
+const arr1 = [15, 12, 11, 29, 5];
+const arr2 = [13, 2, 6, 7];
+*/
